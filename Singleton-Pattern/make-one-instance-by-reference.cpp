9514beaa0e9d36a846731e99f092be3faf654c14
utkarsh prakash <114710547+utkarshpra78@users.noreply.github.com>
2024-07-31 22:19:05 +0530

    Create make-one-instance-by-reference.cpp

---
+++ Singleton-Pattern/make-one-instance-by-reference.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Logger{
+    public:
+    //retrive the single instance of the object
+    //created and we will enforce only 1 of these
+    //object gets created in our code logic
+    static Logger& GetInstance(){
+        static Logger* s_instance=new Logger; 
+        return *s_instance;
+    }
+
+    void printMessage(){
+        cout<<"Acessing the log"<<endl;
+        for(auto it:m_messages){
+            cout<<it<<endl;
+        }
+    }
+
+    void addMessage(string s){
+        m_messages.push_back(s);
+    }
+
+	private: 
+	Logger(){
+        cout<<"Logger was "<<endl; 
+    }
+
+    ~Logger(){
+        //prints when we leave the scope of main
+        cout<<"Logger was destructed"<<endl;
+    }
+ 
+    vector<string>m_messages;
+}; 
+
+
+
+int main() { 
+    // Logger log1; 
+    // Logger log2; 
+    // Logger log3; 
+    // Logger log4; 
+    Logger::GetInstance().printMessage();
+    Logger::GetInstance().addMessage("hi there 1");
+    Logger::GetInstance().addMessage("hi there 2");
+    Logger::GetInstance().addMessage("hi there 3");
+    Logger::GetInstance().addMessage("hi there 4");
+    Logger::GetInstance().printMessage();
+
+    // Logger* pointer_to_logger = Logger::GetInstance();
+    // delete(pointer_to_logger); 
+    return 0;
+}
